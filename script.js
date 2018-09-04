5c3d52251397e41ee2a798a68182fefcb5d2eb22
russellicious <42648750+russellicious@users.noreply.github.com>
2018-09-04 14:28:10 -0500

    Added basic style for mobile vertical, added Features, faq's and contact links

--- script.js
+++ script.js
@@ -7,3 +7,27 @@ function myFunction() {
     x.style.display = "block";
   }
 }
+
+// scroll reveal, not working like I want, gets hung up and does not display smoothly for somereason
+// ScrollReveal().reveal('.ideabox');
+
+// accordion sidebar
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+        // set to toggle off after no longer on? maybe use mouseenter and then set if no mouse enter somehow?
+
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
